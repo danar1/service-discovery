3f24c022609d810a08ba31970f1cb7c17447e333
danar1 <danar1@users.noreply.github.com>
2020-11-28 23:15:07 +0200

    Provision instances in different availability zones

--- homework/hw1/terraform/instances.tf
+++ homework/hw1/terraform/instances.tf
@@ -1,11 +1,10 @@
 resource "aws_instance" "consul_server" {
 
-  count                  = var.consul_servers_count
-  subnet_id = var.subnet_id
-
-  ami           = data.aws_ami.ubuntu-18.id
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.service_discovery_key.key_name
+  count             = var.consul_servers_count
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  ami               = data.aws_ami.ubuntu-18.id
+  instance_type     = "t2.micro"
+  key_name          = aws_key_pair.service_discovery_key.key_name
   associate_public_ip_address = true
 
   iam_instance_profile   = aws_iam_instance_profile.consul-join.name
@@ -20,16 +19,15 @@ resource "aws_instance" "consul_server" {
 
 resource "aws_instance" "consul_agent" {
 
-  count                  = var.consul_agent_count
-  subnet_id = var.subnet_id
+  count             = var.consul_agent_count
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
-  ami           = data.aws_ami.ubuntu-18.id
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.service_discovery_key.key_name
+  ami               = data.aws_ami.ubuntu-18.id
+  instance_type     = "t2.micro"
+  key_name          = aws_key_pair.service_discovery_key.key_name
   associate_public_ip_address = true
-
-  iam_instance_profile   = aws_iam_instance_profile.consul-join.name
-  vpc_security_group_ids = [aws_security_group.opsschool_consul.id]
+  iam_instance_profile        = aws_iam_instance_profile.consul-join.name
+  vpc_security_group_ids      = [aws_security_group.opsschool_consul.id]
 
   tags = {
     Name = "opsschool-agent"
